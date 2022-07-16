038632551c1655c21bfffefafb95f2290b33d575
raghv <raghv@DESKTOP-C50TDGB>
2022-07-16 12:58:21 +0530

    commit: thanks

---
+++ MyProject/src/Test/Testcase.java
@@ -0,0 +1,14 @@
+package Test;
+
+public class Testcase {
+
+	public static void main(String[] args) 
+	{
+     
+		System.out.println("Hello dear");
+		System.out.println("Hello MONU");
+		System.out.println("Hello KISHAN");
+		
+	}
+
+}
